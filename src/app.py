abc360570683ef4a181b978cab6bd3b548182d29
Jose A Muñoz Cardozo <119539448+joshemcr2@users.noreply.github.com>
2024-02-10 14:14:19 +0000

    first commit.

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -36,15 +36,144 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
 
-    return jsonify(response_body), 200
+    return jsonify(all_users), 200
 
+@app.route('/users/<int:theid>', methods=['GET'])
+def users_by_id(theid=None):
+    users = User.query.get(theid)
+    if users is None:
+        return jsonify({"Message": "User does not exist yet"}), 404 
+
+    return jsonify(users.serialize(), 200)
+
+@app.route('/people', methods=['GET'])
+def list_characters():
+    people = People.query.all()
+    people_list =[]
+    for item in people:
+        people_list.append(item.serialize())
+    return jsonify(people_list)
+
+@app.route('/people/<int:theid>', methods=['GET'])
+def characters_by_id(theid=None):
+    people = People.query.get(theid)
+    if people is None:
+        return jsonify({"Message": "People does not exist yet"})
+
+    return jsonify(people.serialize())
+
+@app.route('/planets', methods=['GET'])
+def planets_list():
+    planet = Planets.query.all()
+    planet_list = []
+    for item in planet:
+        planet_list.append(item.serialize())
+    return jsonify(planet_list)
+
+# planets_id
+@app.route('/planets/<int:theid>', methods=['GET'])
+def planets_by_id(theid=None):
+    planet = Planets.query.get(theid)
+    if planet is None:
+        return jsonify({"Message":"Planet does not exist yet"}), 404
+    else:
+        return jsonify(planet.serialize())
+    
+@app.route('/user/favorites/<int:theid>', methods=['GET'])
+def get_user_favorites(theid=None):
+    if theid is None:
+        return jsonify({"Message":"User not found"})
+     
+    
+    favorites = Favorite.query.filter_by(user_id=theid).all()
+    if favorites is None:
+        return jsonify({"Message":"This user has no favorites"})
+    favorites_list = []
+    for item in favorites:
+        favorites_list.append(item.serialize())
+    return jsonify(favorites_list), 200
+
+@app.route('/favorites/planets/<int:planets_number>/<int:user_number>', methods=['POST'])
+def add_planets_favorites(planets_number, user_number): 
+    favorite = Favorite.query.filter_by(planets_id = planets_number, user_id = user_number).first()
+
+    user = User.query.get(user_number)
+
+    if user is None:
+        return jsonify({"Message":"User does not exist yet"}), 404
+
+    if favorite is not None:
+        return jsonify({"Message":"This favorite already exist"})
+
+    add_favorite = Favorite(user_id = user_number, planets_id = planets_number)
+    db.session.add(add_favorite)
+    
+
+    try:
+        db.session.commit()
+        return jsonify({"Message":"The favorite was added"}), 200
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"Message":f"{error}"})
+    
+@app.route('/favorites/people/<int:people_number>/<int:user_number>', methods=['POST'])
+def add_people_favorites(people_number, user_number):
+    favorite = Favorite.query.filter_by(user_id = user_number, people_id = people_number).first()
+
+    user = User.query.get(user_number)
+
+    if user is None:
+        return jsonify({"Message":"User does not exist yet"})
+    
+    if favorite is not None:
+        return jsonify({"Message":"This favorite already exist"})
+    
+    add_favorite = Favorite(user_id = user_number, people_id = people_number)
+    db.session.add(add_favorite)
+
+    try:
+        db.session.commit()
+        return jsonify({"Message":"Favorite was added"})
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"Message":f"{error}"}), 500
+
+@app.route('/favorites/planets/<int:planets_number>/<int:user_number>', methods=['DELETE'])
+def delete_favorite(planets_number, user_number):
+    favorite = Favorite.query.filter_by(user_id = user_number, planets_id = planets_number).first()
+
+    if favorite is None:
+        return jsonify({"Message":"Favorite does not exist"}), 404
+
+    try:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({"Message":"This favorite was deleted"}), 200
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"Message":f"{error}"}), 500
+    
+@app.route('/favorites/people/<int:people_number>/<int:user_number>', methods=['DELETE'])
+def delete_people_favorite(people_number, user_number):
+    favorite = Favorite.query.filter_by(user_id = user_number, people_id = people_number).first()
+
+    if favorite is None:
+        return jsonify({"Message":"Favorite does not exist"}), 404
+    
+    try:
+        db.session.delete(favorite)
+        db.session.commit()
+        return jsonify({"Message":"This favorite was deleted"}), 200
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"Message":f"{error}"}, 500)
+    
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
